1d461e8414d47a70647abcac3603aef000fd72e7
Phil <oilsinwater@gmail.com>
2017-01-04 21:42:14 -0800

    Add files from previous /msctech.info

---
+++ public/app.js
@@ -0,0 +1,26 @@
+  (function ()  {
+    angular
+      .module("BlogApp", [])
+      .controller("BlogController", BlogController);
+
+    function BlogController($scope, $http) {
+      $scope.createPost = createPost;
+
+      function init() {
+        getAllPosts();
+      }
+
+      function getAllPosts() {
+        $http
+            .get("/api/blogpost");
+            .success(function(posts){
+                $scope.posts = posts;
+            })
+      }
+
+      function createPost(post) {
+        console.log(post);
+        $http.post("/api/blogpost", post);
+      }
+    }
+})();
---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/blogfall2016');
+
+var PostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  body: String,
+  tag: {type: String, enum: ['POLITICS', 'EDUCATION', 'ECONOMY']},
+  posted: {type: Date, default: Date.now}
+}, {collection: 'post'});
+
+var PostModel = mongoose.model("PostModel", PostSchema)
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/blogpost", createPost);
+app.get("/api/blogpost", getAllPosts);
+  function getAllPosts(req, res){
+    PostModel
+        .find()
+        .then(
+          function(posts){
+            res.json(posts);
+          },
+          function(err){
+            res.sendStatus(400);
+          }
+        );
+  }
+
+  function createPost(req, res) {
+    var post = req.body;
+    console.log(post);
+    PostModel
+        .create(post)
+        .then(
+            function(postObj) {
+              res.json(200);
+            },
+            function(error) {
+              res.sendStatus(400);
+            }
+        );
+}
+
+app.listen(3000);
