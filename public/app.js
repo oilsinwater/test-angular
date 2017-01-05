76c25f3fc0216c5d6c903016542b3fead06202b0
Phil <oilsinwater@gmail.com>
2017-01-04 23:35:17 -0800

    adding comments with syntax error in app.js

--- public/app.js
+++ public/app.js
@@ -1,26 +1,33 @@
   (function ()  {
     angular // declares app is an angular application
-      .module("BlogApp", []) // see index.html ln 2, app must be declared
-      .controller("BlogController", BlogController);
+        .module("BlogApp", []) // see index.html ln 2, app must be declared
+        .controller("BlogController", BlogController);
 
     function BlogController($scope, $http) {
       $scope.createPost = createPost;
+      // $scope service is used to be able to interact with html template, send and receives both data and event handlers
+      // $http service allows app to communicate between browser and server, requests and responses
 
       function init() {
         getAllPosts();
       }
+      init();
 
       function getAllPosts() {
         $http
             .get("/api/blogpost");
+            // reading blogposts
             .success(function(posts)){
                 $scope.posts = posts;
             })
       }
+      // function to retrieve the latest blog posts
 
       function createPost(post) {
+      // function above holds an object that contain the title and body
         console.log(post);
         $http.post("/api/blogpost", post);
+        // listening to the server, then creating new instances of posts
       }
     }
 })();
--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 var express = require('express');
 var app = express();
+// to pass requests and responses
 var bodyParser = require('body-parser');
+// used to parse the body, i.e. json
 
 var mongoose = require('mongoose');
+// load the library
 mongoose.connect('mongodb://localhost/blogfall2016');
+// connects the instance to the database, can use Schemas and Models...
 
 var PostSchema = mongoose.Schema({
   title: {type: String, required: true},
+  // note "{type: ...}" configures as an object with values, functions, etc..
   body: String,
-  tag: {type: String, enum: ['POLITICS', 'EDUCATION', 'ECONOMY']},
+  tag: {type: String, enum: ['POLITICS', 'EDUCATION', 'ECO  NOMY']},
   posted: {type: Date, default: Date.now}
 }, {collection: 'post'});
+// creates validation, makes structure for the data that's fetched or entered
+// names mongo dbs collection
 
 var PostModel = mongoose.model("PostModel", PostSchema)
+// actually API that allows interaction with the database--delete, add, edit
+// validated against the PostSchema
 
 app.use(express.static(__dirname + '/public'));
+// location where static info is held, facing the user
 app.use(bodyParser.json());
+// turns on the json parser
 app.use(bodyParser.urlencoded({ extended: true }));
-
+// parses json out of the body
 app.post("/api/blogpost", createPost);
+// listening to request, mapped to local function to handle request
+
 app.get("/api/blogpost", getAllPosts);
+// listening for incoming get requests
+
   function getAllPosts(req, res){
     PostModel
         .find()
@@ -32,6 +47,7 @@ app.get("/api/blogpost", getAllPosts);
           }
         );
   }
+  // retrieves all the posts from the database and sends it back to the client
 
   function createPost(req, res) {
     var post = req.body;
@@ -45,7 +61,10 @@ app.get("/api/blogpost", getAllPosts);
             function(error) {
               res.sendStatus(400);
             }
+            // returns either a successful and failed callback function
         );
 }
+// local function implemented to recieves a request and response,
+// so it can participate in request response cycle between the client and the server
 
 app.listen(3000);
